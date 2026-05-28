9addc76edbbefb7a501eb87d95ca7e6c26b4a9b2
Kaio <kaiomascarenhas241@gmail.com>
2026-05-28 20:35:46 -0300

    update

--- Atividade_de_recuperacao/Form1.Designer.cs
+++ Atividade_de_recuperacao/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(546, 318);
+            ClientSize = new Size(706, 379);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
